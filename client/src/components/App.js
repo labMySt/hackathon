274dc7673a1b0e407a1934d401f8e3c58ecb160d
Vasyl Savitskyy <vasylsavitskyy@gmail.com>
2018-06-30 15:47:40 +0300

    add header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Header from "./landing/Header";
+import Header from "./landing/header/Header";
 import Footer from "./landing/footer/Footer";
 
 class App extends Component {
